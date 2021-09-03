dce7e5192d27345233ed6cee9c0f1cfd6deb8446
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-04 00:50:14 +0530

    Accept page updated

--- pdf/models.py
+++ pdf/models.py
@@ -3,11 +3,21 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=100)
+    linkedin = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     email = models.EmailField()
+    github = models.CharField(max_length=100)
+
     school = models.CharField(max_length=100)
     degree = models.CharField(max_length=100)
-    university = models.CharField(max_length=100)
+    passyear = models.CharField(max_length=100)
+    cgpa = models.CharField(max_length=100)
+
+    post = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+
     skills = models.TextField(max_length=1000)
-    about = models.TextField(max_length=1000)
-    previousWork = models.TextField(max_length=1000)
+
+    achievements = models.TextField(max_length=1000)
+
+    extracurr = models.TextField(max_length=1000)
