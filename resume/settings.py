2dabd5727b1331d7d11685a81bf56e7b38671ef4
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-08 00:49:23 +0530

    Day 6.2

--- resume/settings.py
+++ resume/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&8-(%cwef$q*ou_kx*r7a1!elyn(69+hf10z70$d)_v788jo8w'
+SECRET_KEY = 'resumeGenKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
